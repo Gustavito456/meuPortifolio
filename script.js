42e2b5992080042e73e9f54fd71cee43dd041bd8
João Gustavo <131564641+Gustavito456@users.noreply.github.com>
2025-04-03 20:37:49 -0300

    v4

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const toggleThemeButton = document.getElementById('toggle-theme');
 const sections = document.querySelectorAll('section');
+const initializeTheme = () => {
+    const savedTheme = localStorage.getItem('theme');
+    document.body.classList.toggle('dark-mode', savedTheme === 'dark');
+    updateThemeIcon();
+    updateFavicon();
+}
+const updateFavicon = () => {
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    const favicon = document.querySelector("link[rel='icon']");
+    const color = isDarkMode ? '81d4fa' : '007bff';
+
+    favicon.href = `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>
+        <style>
+            .bg { fill: %23${color}; }
+            .text { fill: %23fff; font-family: sans-serif; font-size: 50px; }
+        </style>
+        <rect class='bg' width='100' height='100' rx='15'/>
+        <text class='text' x='50%' y='68%' text-anchor='middle'>JG</text>
+    </svg>`;
+};
 let themeClicks = 0;
 
 const updateThemeIcon = () => {
@@ -9,13 +29,15 @@ const updateThemeIcon = () => {
 
 const handleThemeToggle = () => {
     document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
     themeClicks++;
-    updateThemeIcon();
 
     if (themeClicks === 5) {
         window.open('https://www.youtube.com/@desenhandoamatematica4772', '_blank');
         themeClicks = 0;
     }
+    updateFavicon();
+    updateThemeIcon();
 };
 
 const toggleSection = (section) => {
@@ -27,6 +49,7 @@ const toggleSection = (section) => {
 
 document.addEventListener('DOMContentLoaded', () => {
     updateThemeIcon();
+    initializeTheme();
 
     sections.forEach(section => {
         const title = section.querySelector('h2');
